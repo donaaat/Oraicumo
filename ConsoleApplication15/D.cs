45a24dec085396f4dca088a8f6077e051b42ad3c
donaaat <donaaaaat@gmail.com>
2017-02-23 12:35:26 +0100

    Add project files.

---
+++ ConsoleApplication15/D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication15
+{
+    public class D : B
+    {
+        public void S7()
+        {
+            Console.WriteLine("D::S7");
+            S3();
+        }
+
+        public void S8()
+        {
+            Console.WriteLine("D::S7");
+            V2();
+        }
+
+        protected virtual void V2()
+        {
+            Console.WriteLine("D::V2");
+            S7();
+        }
+    }
+}
